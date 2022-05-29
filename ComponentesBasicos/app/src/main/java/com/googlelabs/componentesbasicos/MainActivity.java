3592878f4a7237efb4eeaedcf1b8d3bc87498570
Kaioleite <kaioleite1998@gmail.com>
2022-05-29 13:08:03 -0300

    Botão de Opção

--- ComponentesBasicos/app/src/main/java/com/googlelabs/componentesbasicos/MainActivity.java
+++ ComponentesBasicos/app/src/main/java/com/googlelabs/componentesbasicos/MainActivity.java
@@ -6,36 +6,86 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
-   // Está chamando o Tipo de cada componente para poder chamar no FindVIewById.
+   // Caixa de Texto
     private EditText campoNome;
     private TextInputEditText campoEmail;
     private TextView textResultado;
-
+   // CheckBox
     private CheckBox checkVerde, checkBranco, checkVermelho;
+
+   //RadioButton
+    private RadioButton sexoMasculino, sexoFeminino;
+    private RadioGroup opcaoSexo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Criamos Acesso para toda parte do Código
+        // Criamos Acesso para toda parte do Código
         campoNome      = findViewById(R.id.editNome);
         campoEmail     = findViewById(R.id.editEmail);
         textResultado  = findViewById(R.id.textResultado);
 
-    //CheckBox - Acessando de toda parte do código
+        //CheckBox - Acessando de toda parte do código
         checkVerde     = findViewById(R.id.checkVerde);
         checkBranco    = findViewById(R.id.checkBranco);
         checkVermelho  = findViewById(R.id.checkVermelho);
 
+        //RadioButton
+        sexoMasculino  = findViewById(R.id.radioButtonM);
+        sexoFeminino   = findViewById(R.id.radioButtonF);
+        //RadioGroup
+         opcaoSexo     = findViewById(R.id.radioGroupSexo);
+         radiobutton();
 
       }
-     public void checkBox(){
+     public void radiobutton(){
+        opcaoSexo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.radioButtonM){
+                    textResultado.setText("Masculino");
+                }else if (checkedId == R.id.radioButtonF){
+                    textResultado.setText("Feminino");
+                }
+            }
+        });
+      /*
+        if (sexoMasculino.isChecked()) {
+            textResultado.setText("Masculino Selecionado");
+        }
+       else { sexoFeminino.isChecked();
+            textResultado.setText("Feminino Selecionado - ");
+        }
+       */
+       //Criando Ouvinte para quando selecionar já mude o texto abaixa (Feminino/Masculino)
+
+     }
+  //enviar - Criamos um Método que ao clicar no botão recupera os dados e guarda na caixa de Resultado.
+    public void enviar(View view){
+        // Método CheckBox chamado no botão enviar
+       // checkBox();
+       //  radiobutton();
+
+      /*
+        String nome = campoNome.getText().toString();
+        String Email = campoEmail.getText().toString();
+        textResultado.setText("nome: "+ nome +"  email: " + Email);
+       */
+   }
+
+
+    public void checkBox(){
 
         String texto = "";
         // checkBox - Recuperando caixa Selecionada [x]
@@ -48,33 +98,21 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-         if(checkBranco.isChecked()){
-             texto = texto + "Branco selecionado - ";
+        if(checkBranco.isChecked()){
+            texto = texto + "Branco selecionado - ";
 
-         }
+        }
 
-         if(checkVermelho.isChecked()){
-             texto = texto + "Vermelho selecionado - ";
+        if(checkVermelho.isChecked()){
+            texto = texto + "Vermelho selecionado - ";
 
-         }
+        }
 
         textResultado.setText(texto);
 
 
 
-     }
-  //enviar - Criamos um Método que ao clicar no botão recupera os dados e guarda na caixa de Resultado.
-    public void enviar(View view){
-        // Método CheckBox chamado no botão enviar
-        checkBox();
-
-
-      /*
-        String nome = campoNome.getText().toString();
-        String Email = campoEmail.getText().toString();
-        textResultado.setText("nome: "+ nome +"  email: " + Email);
-       */
-   }
+    }
    /* limpar - Criamos um método no botão limpar para que ele apague
        o que foi digitado dentro da caixa de texto */
 
